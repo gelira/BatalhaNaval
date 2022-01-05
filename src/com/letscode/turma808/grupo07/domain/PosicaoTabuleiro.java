d668ba4e3ac7b288c9e967a3b27f5e2723827676
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-01-05 07:19:31 -0300

    classe domínio PosicaoTabuleiro

---
+++ src/com/letscode/turma808/grupo07/domain/PosicaoTabuleiro.java
@@ -0,0 +1,13 @@
+package com.letscode.turma808.grupo07.domain;
+
+public class PosicaoTabuleiro {
+    private boolean navioPosicionado;
+    private boolean tiro;
+    private boolean tiroCerteiro;
+
+    public PosicaoTabuleiro() {
+        navioPosicionado = false;
+        tiro = false;
+        tiroCerteiro = false;
+    }
+}
