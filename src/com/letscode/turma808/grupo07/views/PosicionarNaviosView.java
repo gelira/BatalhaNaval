04f9c8dc326d219225a89dbfea4faac6460230d7
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-01-09 14:44:33 -0300

    ask posição

--- src/com/letscode/turma808/grupo07/views/PosicionarNaviosView.java
+++ src/com/letscode/turma808/grupo07/views/PosicionarNaviosView.java
@@ -5,6 +5,41 @@ import com.letscode.turma808.grupo07.domain.Tabuleiro;
 import java.util.Scanner;
 
 public class PosicionarNaviosView {
+    public static void showDescricao() {
+        System.out.println("As posições devem ser informadas no formato [A-H][0-9].");
+    }
+
+    public static void showMensagemPosicaoInvalida() {
+        System.out.println("A posição escolhida não existe ou já tem um navio nela. Escolha novamente.");
+    }
+
+    public static Integer[] askPosicao() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Informe onde quer posicionar o navio: ");
+        String p = sc.next();
+
+        if (p.length() != 2) {
+            return new Integer[] { null, null };
+        }
+
+        char linha = p.charAt(0);
+        char coluna = p.charAt(1);
+
+        int linhaIndex = Tabuleiro.LINHAS.indexOf(linha);
+        int colunaIndex = Tabuleiro.COLUNAS.indexOf(coluna);
+
+        Integer[] posicao = new Integer[] { null, null };
+
+        if (linhaIndex >= 0) {
+            posicao[0] = linhaIndex;
+        }
+        if (colunaIndex >= 0) {
+            posicao[1] = colunaIndex;
+        }
+
+        return posicao;
+    }
 
     public static String posicao[][]  = new String[10][10];//posicoes dos barcos no tabuleiro//
     int linha, coluna;
