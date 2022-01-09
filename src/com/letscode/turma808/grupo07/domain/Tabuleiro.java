747d1f27fc7ac5e8230f73c1664a98a9b067160f
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-01-09 10:02:00 -0300

    getters e setters Tabuleiro

--- src/com/letscode/turma808/grupo07/domain/Tabuleiro.java
+++ src/com/letscode/turma808/grupo07/domain/Tabuleiro.java
@@ -1,5 +1,7 @@
 package com.letscode.turma808.grupo07.domain;
 
+import java.util.Random;
+
 public class Tabuleiro {
     public static final int QUANTIDADE_NAVIOS = 10;
     public static final String LINHAS = "ABCDEFGHIJ";
@@ -20,5 +22,38 @@ public class Tabuleiro {
                 posicoes[i][j] = new PosicaoTabuleiro();
             }
         }
+    }
+
+    public String getNomeJogador() {
+        return nomeJogador;
+    }
+
+    public int getQuantidadeNaviosRestantes() {
+        return quantidadeNaviosRestantes;
+    }
+
+    public PosicaoTabuleiro[][] getPosicoes() {
+        return posicoes;
+    }
+
+    public PosicaoTabuleiro getPosicao() {
+        Random r = new Random();
+        return getPosicao(r.nextInt(LINHAS.length()), r.nextInt(COLUNAS.length()));
+    }
+
+    public PosicaoTabuleiro getPosicao(String linha, String coluna) {
+        return getPosicao(LINHAS.indexOf(linha), COLUNAS.indexOf(coluna));
+    }
+
+    private PosicaoTabuleiro getPosicao(int linhaIndex, int colunaIndex) {
+        if (linhaIndex < 0 || colunaIndex < 0) {
+            return null;
+        }
+
+        return posicoes[linhaIndex][colunaIndex];
+    }
+
+    public void setQuantidadeNaviosRestantes(int quantidadeNaviosRestantes) {
+        this.quantidadeNaviosRestantes = quantidadeNaviosRestantes;
     }
 }
