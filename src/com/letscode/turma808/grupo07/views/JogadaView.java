925818991cdfb5015185d7e1d3ac75b545a3154f
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-01-10 15:21:36 -0300

    view de jogada

--- src/com/letscode/turma808/grupo07/views/JogadaView.java
+++ src/com/letscode/turma808/grupo07/views/JogadaView.java
@@ -1,56 +1,44 @@
 package com.letscode.turma808.grupo07.views;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import com.letscode.turma808.grupo07.domain.Tabuleiro;
+
+import java.util.Scanner;
 
 public class JogadaView {
-
-    public static int posicao[][]  = new int[10][10];//posicoes dos barcos no tabuleiro//
-
-    public static void zeraPosicao(){
-        for(int l=0;l<10;l++){//preenche as linhas com 0//
-            for(int c=0;c<10;c++){//preenche as colunas com 0//
-                posicao[l][c] = 0;
-            }
-        }
+    public static void showPosicaoInvalida() {
+        System.out.println("Posição selecionada é inválida.");
     }
 
-
-    public static int lerLinha(){
-        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
-        int linha = 0;
-        try {
-            System.out.println("Informe a linha: ");
-            linha = Integer.parseInt(entrada.readLine());
-            if(linha >=0 && linha < 10){
-                return(linha);
-            }else{
-                System.out.println("Linha invalida!!!");
-                return(lerLinha());
-            }
-        } catch(Exception e){
-            System.out.println("Linha invalida!!!");
-            return(lerLinha());
-        }
+    public static void showTiroAnterior() {
+        System.out.println("Você já atirou na posição selecionada.");
     }
 
-    public static int lerColuna(){
-        BufferedReader entrada = new BufferedReader (new InputStreamReader(System.in));
-        int coluna = 0;
+    public static Integer[] askPosicao() {
+        Scanner sc = new Scanner(System.in);
 
-        try {
-            System.out.println("Informe a coluna: ");
-            coluna = Integer.parseInt(entrada.readLine());
-            if(coluna >= 0 && coluna < 10){
-                return(coluna);
-            }else{
-                System.out.println("Coluna invalida!!!");
-                return(lerColuna());
-            }
-        } catch(Exception e){
-            System.out.println("Coluna invalida!!!");
-            return(lerColuna());
+        System.out.print("Onde deseja atirar? Informe a posição no formato [A-H][0-9]: ");
+        String p = sc.next().toUpperCase();
+
+        if (p.length() != 2) {
+            // Posição inválida
+            return null;
         }
-    }
 
+        char linha = p.charAt(0);
+        char coluna = p.charAt(1);
+
+        int linhaIndex = Tabuleiro.LINHAS.indexOf(linha);
+        int colunaIndex = Tabuleiro.COLUNAS.indexOf(coluna);
+
+        Integer[] posicao = new Integer[] { null, null };
+
+        if (linhaIndex >= 0) {
+            posicao[0] = linhaIndex;
+        }
+        if (colunaIndex >= 0) {
+            posicao[1] = colunaIndex;
+        }
+
+        return posicao;
+    }
 }
--- src/com/letscode/turma808/grupo07/views/PosicionarNaviosView.java
+++ src/com/letscode/turma808/grupo07/views/PosicionarNaviosView.java
@@ -46,38 +46,4 @@ public class PosicionarNaviosView {
 
         return posicao;
     }
-
-    public static String posicao[][]  = new String[10][10];//posicoes dos barcos no tabuleiro//
-    int linha, coluna;
-
-
-    public String[][] posicionarNavios(){
-        System.out.println("Informe as posições que deseja colocar seus navios");
-
-        for (int x = 0; x < 10;){
-            linha = JogadaView.lerLinha();
-            coluna = JogadaView.lerColuna();
-
-            if(posicao[linha][coluna] == null){
-                posicao[linha][coluna] = "N";
-                x++;
-            }else{
-                System.out.println("Posição já informada. Informe outras posições.");
-            }
-        }
-        return posicao;
-    }
-
-    public String[][] posicionarNaviosAutomatico(){
-        for (int x = 0; x < 10;){
-            linha = (int)(Math.random() * 9) + 1;
-            coluna = (int)(Math.random() * 9) + 1;
-
-            if(posicao[linha][coluna] == null){
-                posicao[linha][coluna] = "N";
-                x++;
-            }
-        }
-        return posicao;
-    }
 }
