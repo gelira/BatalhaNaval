1b78bc02802579c9d9125d05a0da13ab7061ee0a
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-01-10 16:42:08 -0300

    alterando nome da variável

--- src/com/letscode/turma808/grupo07/controllers/JogoController.java
+++ src/com/letscode/turma808/grupo07/controllers/JogoController.java
@@ -1,16 +1,16 @@
 package com.letscode.turma808.grupo07.controllers;
 
 public class JogoController {
-    private TabuleiroController tabuleiroJogador;
+    private TabuleiroController tabuleiroHumano;
     private TabuleiroController tabuleiroComputador;
 
     public JogoController() {
-        tabuleiroJogador = new TabuleiroController();
+        tabuleiroHumano = new TabuleiroController();
         tabuleiroComputador = new TabuleiroController("COMPUTADOR");
     }
 
     public void posicionarNavios() {
-        tabuleiroJogador.definirPosicoesNavios();
+        tabuleiroHumano.definirPosicoesNavios();
         tabuleiroComputador.definirPosicoesNaviosAleatoriamente();
     }
 
