a4c47b1ed37b2690d2d1526199cebaf326977f4b
Geraldo Eloi <geraldo.eloi11@gmail.com>
2022-01-10 15:09:42 -0300

    Iniciando jogo

--- src/com/letscode/turma808/grupo07/controllers/JogoController.java
+++ src/com/letscode/turma808/grupo07/controllers/JogoController.java
@@ -1,4 +1,18 @@
 package com.letscode.turma808.grupo07.controllers;
 
 public class JogoController {
+    private TabuleiroController tabuleiroJogador;
+    private TabuleiroController tabuleiroComputador;
+
+    public JogoController() {
+        tabuleiroJogador = new TabuleiroController();
+        tabuleiroComputador = new TabuleiroController("COMPUTADOR");
+    }
+
+    public void posicionarNavios() {
+        tabuleiroJogador.definirPosicoesNavios();
+        tabuleiroComputador.definirPosicoesNaviosAleatoriamente();
+    }
+
+
 }
